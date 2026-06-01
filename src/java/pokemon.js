72eef2dcdb229b99e116360af2dd353f368384f0
Eva Stam <evajudithstam@live.nl>
2026-06-01 15:17:35 +0200

    ophalen pokeapi

---
+++ src/java/pokemon.js
@@ -0,0 +1,15 @@
+//Dit in aparte folder, zodat ik deze code kan ophalen zowel in de pokédex, als de teams.
+
+export async function getPokemon() {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
+
+    const data = await response.json();
+
+    const pokemon = await Promise.all(data.results.map(async (poke) => {
+        const detailResponse = await fetch(poke.url);
+        return detailResponse.json();
+        })
+    );
+
+    return pokemon;
+}
